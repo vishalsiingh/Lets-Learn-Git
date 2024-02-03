8e204abcd2471510ccbafdb63749d8c6da5acb6b
Vishal singh kashyap <singhvishalk165@gmail.com>
2024-02-03 12:36:22 +0530

    adding multi.java

---
+++ multi.java
@@ -0,0 +1,9 @@
+
+    public class multi{
+        public static void main(String[] args) {
+            int a=4;
+            int b=6;
+            System.out.println("Product  is :"+(a*b));
+        }
+    }
+
